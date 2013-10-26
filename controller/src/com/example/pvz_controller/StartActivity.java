9bfda762dde4085cb5cc1b0dd3a9405e6cba686b
Kyle Kelly <strfey16@gmail.com>
2013-10-25 22:29:13 -0400

    Hackily connecting and sending data to Jarvis' server

--- controller/src/com/example/pvz_controller/StartActivity.java
+++ controller/src/com/example/pvz_controller/StartActivity.java
@@ -1,14 +1,18 @@
 package com.example.pvz_controller;
 
-import java.util.Arrays;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
+import android.hardware.SensorEventListener ;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
+import android.util.Log;
 import android.view.Menu;
 import android.widget.TextView;
 
@@ -21,9 +25,10 @@ public class StartActivity extends Activity {
 	private final int RIGHT_THRESHOLD = 25;
 	private final int LEFT_THRESHOLD = 25;
 	private float[] orientationVal = new float[3];
-	private TextView txtOrient;
+//	private TextView txtOrient;
 	private TextView txtDirection;
 	private Direction direction;
+	private Socket s;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -31,10 +36,40 @@ public class StartActivity extends Activity {
 		setContentView(R.layout.activity_start);       
 		SensorManager sensorManager = (SensorManager) this.getSystemService(Context.SENSOR_SERVICE);
 
-//      txtRotVec = (TextView) findViewById(R.id.rotVec);
-//		txtOrient = (TextView) findViewById(R.id.orientation);
+		//      txtRotVec = (TextView) findViewById(R.id.rotVec);
+		//		txtOrient = (TextView) findViewById(R.id.orientation);
 		txtDirection = (TextView) findViewById(R.id.direction);
 		direction = Direction.STOPPED;
+		
+		new Thread(new Runnable(){
+			public void run(){
+				try{
+					s = new Socket("143.215.104.172", 4000);
+				}catch(UnknownHostException e){
+					Log.e("Kyle", "UnkownHostExcpetion happened on connect");
+				} catch(IOException ex){
+					Log.e("Kyle", "IOException happened on connect");
+					Log.e("Kyle", ex.getMessage());
+				}
+
+				Direction lastSent = Direction.STOPPED;
+				while(true){
+					if(lastSent != direction){
+						if(s == null)
+							Log.e("Kyle", "Shit broke");
+						try{
+							PrintWriter sockWriter = new PrintWriter(s.getOutputStream(), true);
+							sockWriter.println(direction.name());
+							lastSent = direction;
+						} catch(IOException ex){
+							Log.e("Kyle", "IOException happened on send");
+							Log.e("Kyle", ex.getMessage());
+						}
+					}
+				}
+			}
+		}).start();
+
 
 		final SensorEventListener mEventListener = new SensorEventListener() {
 			public void onAccuracyChanged(Sensor sensor, int accuracy) {
@@ -46,20 +81,20 @@ public class StartActivity extends Activity {
 				case Sensor.TYPE_ORIENTATION:
 					System.arraycopy(event.values, 0, orientationVal, 0, 3);
 					break;
-//              case Sensor.TYPE_ROTATION_VECTOR:
-//                  System.arraycopy(event.values, 0, rotVecVal, 0, 4);
-//                  break;
+					//              case Sensor.TYPE_ROTATION_VECTOR:
+					//                  System.arraycopy(event.values, 0, rotVecVal, 0, 4);
+					//                  break;
 				}
-				txtOrient.setText("Orientation: " + Arrays.toString(orientationVal));
+				//				txtOrient.setText("Orientation: " + Arrays.toString(orientationVal));
 				determineDirection();
 				txtDirection.setText("Direction: " + direction.name());
-//              txtRotVec.setText("RotVec: " + Arrays.toString(rotVecVal));
+				//              txtRotVec.setText("RotVec: " + Arrays.toString(rotVecVal));
 
 			};
 		};
 		setListners(sensorManager, mEventListener);
 	}
-	
+
 	public void determineDirection(){
 		Direction newDir = direction;
 		if (orientationVal[1] > -130 && orientationVal[1] < 130){
@@ -74,6 +109,8 @@ public class StartActivity extends Activity {
 		} else if (orientationVal[2] < -UP_THRESHOLD){
 			newDir = Direction.UP;
 		}
+		if(!newDir.equals(direction))
+			//SEND STUFF
 		direction = newDir;
 	}
 
